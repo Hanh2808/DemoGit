068c41fdca40a7eb77286ef1a589d334693f8c62
Hanh2808 <85823857+Hanh2808@users.noreply.github.com>
2021-10-05 07:26:46 +0700

    10/5

---
+++ Test Suites/Test Suite_DN.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite_DN</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>70990865-b887-4896-8474-8df7b7ddb6b3</testSuiteGuid>
+</TestSuiteEntity>
